871164bcb7a6d97c85f8b193382b43419210b3f2
lisihocke <lisihocke@users.noreply.github.com>
2019-03-12 16:45:18 +0100

    reformatted tests, extracted test data setup method

--- src/test/java/com/lisihocke/restpractice/EmployeeTests.java
+++ src/test/java/com/lisihocke/restpractice/EmployeeTests.java
@@ -15,21 +15,16 @@ public class EmployeeTests extends TestSetup {
     @BeforeClass
     public static void makeSureThatApplicationIsUp() {
         SerenityRest.
-                when().
-                    get("employees").
-                then().
-                    statusCode(200);
+                when().get("employees").
+                then().statusCode(200);
     }
 
     @Test
     public void getFirstEmployee() {
         SerenityRest.
-                given().
-                    pathParam("id", "1").
-                when().
-                    get("employees/{id}").
-                then().
-                    statusCode(200).
+                given().pathParam("id", "1").
+                when().get("employees/{id}").
+                then().statusCode(200).
                     body("firstName", is("Bilbo")).
                     body("lastName", is("Baggins")).
                     body("role", is("burglar")).
@@ -44,13 +39,9 @@ public class EmployeeTests extends TestSetup {
         requestBody.addProperty("role", "chef");
 
         SerenityRest.
-                given().
-                    contentType("application/json").
-                    body(requestBody.toString()).
-                when().
-                    post("employees").
-                then().
-                    statusCode(201).
+                given().contentType("application/json").body(requestBody.toString()).
+                when().post("employees").
+                then().statusCode(201).
                     body("firstName", is("Cindy")).
                     body("lastName", is("Chef")).
                     body("role", is("chef")).
@@ -59,38 +50,32 @@ public class EmployeeTests extends TestSetup {
 
     @Test
     public void deleteEmployee() {
+        int employeeId = getEmployeeId();
+
+        SerenityRest.
+                given().pathParam("id", employeeId).
+                when().delete("employees/{id}").
+                then().statusCode(204);
+
+        // TODO: can we check that it's really gone now?
+        SerenityRest.
+                given().pathParam("id", employeeId).
+                when().get("employees/{id}").
+                then().statusCode(404);
+        // TODO: assert for the content of the response
+        // TODO: service responds plain text, but we want it to be JSON!
+    }
+
+    private int getEmployeeId() {
         JsonObject requestBody = new JsonObject();
         requestBody.addProperty("firstName", "Betty");
         requestBody.addProperty("lastName", "Baker");
         requestBody.addProperty("role", "baker");
 
         // TODO: can we get a response without g/w/t? this is just setup
-        int employeeId = SerenityRest.
-                given().
-                    contentType("application/json").
-                    body(requestBody.toString()).
-                when().
-                    post("employees").
-                then().
-                    extract().path("id");
-
-        SerenityRest.
-                given().
-                    pathParam("id", employeeId).
-                when().
-                    delete("employees/{id}").
-                then().
-                    statusCode(204);
-
-        // TODO: can we check that it's really gone now?
-        SerenityRest.
-                given().
-                    pathParam("id", employeeId).
-                when().
-                    get("employees/{id}").
-                then().
-                    statusCode(404);
-        // TODO: assert for the content of the response
-        // TODO: service responds plain text, but we want it to be JSON!
+        return SerenityRest.
+                given().contentType("application/json").body(requestBody.toString()).
+                when().post("employees").
+                then().extract().path("id");
     }
 }
